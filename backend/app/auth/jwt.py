ae7d42a6a4dd136b6df1fb12a4a2612d7bb326ae
Moksh Singh <mokshmanral628@gmail.com>
2026-07-18 17:30:30 +0530

    Added /auth/login verify password, issue JWT functionality

---
+++ backend/app/auth/jwt.py
@@ -0,0 +1,29 @@
+from datetime import datetime, timedelta, UTC
+import os
+
+from jose import jwt
+from dotenv import load_dotenv
+
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES = int(
+    os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", "1440")
+)
+
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+
+    expire = datetime.now(UTC) + timedelta(
+        minutes=ACCESS_TOKEN_EXPIRE_MINUTES
+    )
+
+    to_encode.update({"exp": expire})
+
+    return jwt.encode(
+        to_encode,
+        SECRET_KEY,
+        algorithm=ALGORITHM
+    )
--- backend/app/routers/auth.py
+++ backend/app/routers/auth.py
@@ -5,6 +5,9 @@ from app.database import get_db
 from app.models.user import User
 from app.schemas.user import UserCreate, UserResponse
 from app.core.security import hash_password
+from app.auth.hashing import verify_password
+from app.auth.jwt import create_access_token
+from app.schemas.user import UserLogin, Token
 
 router = APIRouter(
     prefix="/auth",
@@ -58,3 +61,44 @@ def signup(
     db.refresh(new_user)
 
     return new_user
+
+@router.post(
+    "/login",
+    response_model=Token
+)
+def login(
+    user: UserLogin,
+    db: Session = Depends(get_db)
+):
+    db_user = (
+        db.query(User)
+        .filter(User.email == user.email)
+        .first()
+    )
+
+    if not db_user:
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid email or password"
+        )
+
+    if not verify_password(
+        user.password,
+        db_user.hashed_password
+    ):
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid email or password"
+        )
+
+    access_token = create_access_token(
+        {
+            "sub": str(db_user.id),
+            "email": db_user.email
+        }
+    )
+
+    return {
+        "access_token": access_token,
+        "token_type": "bearer"
+    }
